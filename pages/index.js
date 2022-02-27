969836d8d141cd97e683dc81cbf5d363ab82d072
João <63080484+joaomadruga@users.noreply.github.com>
2022-02-27 13:26:47 -0300

    fixing deploy in vercel

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,30 @@ import Button from '../components/Button';
 import CentralizedDiv from '../components/CentralizedDiv';
 import ErrorPopup from '../components/ErrorPopup';
 
+export const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
 export async function getServerSideProps() {
 
-  const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
+  const { privateKey } = JSON.parse(process.env.GOOGLE_PRIVATE_KEY || '{ privateKey: null }')
 
-  const sheets = google.sheets({ version: 'v4', auth });
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES,
+    projectId: process.env.GOOGLE_PROJECTID,
+    credentials: {
+      private_key: privateKey,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+    },
+  })
 
+
+  const authToken = await auth.getClient();
+  const sheets = google.sheets({ version: 'v4', authToken });
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
     range: 'B5:D',
+    auth: authToken,
   });
 
   const posts = response.data.values;
